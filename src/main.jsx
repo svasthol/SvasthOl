3c9ae8915f22fda0c5843fc271809b3f421bc6a0
svasthol <svasthol@gmail.com>
2025-10-08 07:34:49 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SvasthOlWebsite from '../svasthol_website_react.jsx'
+import '../index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SvasthOlWebsite />
+  </React.StrictMode>,
+)
