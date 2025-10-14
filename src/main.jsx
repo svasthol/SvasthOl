4638bf4c71e2c95c668c0eb58ebfe6a12c177953
svasthol <svasthol@gmail.com>
2025-10-15 00:04:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SvasthOlWebsite from './src/svasthol_website_react.jsx'
+import SvasthOlWebsite from 'svasthol_website_react.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,4 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 
 )
+
 
