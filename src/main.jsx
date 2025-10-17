752b109f3083da84a308f86d362f9abf5f76d1f3
svasthol <svasthol@gmail.com>
2025-10-17 12:07:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import SvasthOlWebsite from './svasthol_website_react'
-import TermsConditions from './Legal/TermsConditions'
-import PrivacyPolicy from './Legal/PrivacyPolicy'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SvasthOlWebsite from "../svasthol_website_react.jsx";
+import TermsAndConditions from "./pages/TermsAndConditions.jsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SvasthOlWebsite />} />
-        <Route path="/terms" element={<TermsConditions />} />
+        <Route path="/terms" element={<TermsAndConditions />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
