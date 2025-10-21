10de34d7be3ec38b4e7b8d33fe17aa63d5a34c65
svasthol <svasthol@gmail.com>
2025-10-22 00:38:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-  mode: "jit", // ✅ faster builds
-};
+}
