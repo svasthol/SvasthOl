abff9d2a01547a22226c06a0a9400e283ea4aece
svasthol <svasthol@gmail.com>
2025-10-21 18:05:53 +0530

    Update svasthol_website_react.jsx

--- src/svasthol_website_react.jsx
+++ src/svasthol_website_react.jsx
@@ -29,6 +29,15 @@ export default function SvasthOlWebsite() {
   const [cart, setCart] = useState([]);
 	const [showCart, setShowCart] = useState(false);
 
+	const [showHintHidden, setShowHintHidden] = useState(false);
+
+useEffect(() => {
+  // Auto-hide after 3 seconds
+  const timer = setTimeout(() => setShowHintHidden(true), 3000);
+  return () => clearTimeout(timer);
+}, []);
+
+
 
  const toggleCartItem = (item) => {
   setCart((prev) => {
